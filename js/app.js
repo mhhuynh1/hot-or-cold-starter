be10c0e656fc3757b136513edd3dd329f71dee34
Michelle Huynh <michehuynh@yahoo.com>
2015-04-20 09:48:20 -0700

    Added some function names

--- js/app.js
+++ js/app.js
@@ -12,6 +12,39 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	newGame();
+  	$(document).on('click' , 'a.new' , newGame);
+  	
 });
+
+function pickRandomNumber () {
+	return Math.floor((Math.random() * 100) + 1);
+};
+
+function giveFeedback () {
+
+};
+
+function newGame () {
+	//clear out fields
+	var counter = 0;
+	$("#userGuess").val('');
+	$("#guessList").empty(); 
+	$("span#count").text(counter);
+
+	//choose a number
+	var actualNum = pickRandomNumber();
+	console.log(actualNum);
+
+	//user puts in something, assign to var
+	var guess;
+	$("guessButton").click(function(){
+		guess = +$("userGuess").val();
+	});
+
+	//check to see how close, givefeedback
+	//repeat while guess is != number
+	//when they win, givefeedback winner
+};
 
 
