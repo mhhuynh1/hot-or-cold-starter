3969d5067ba7ec5f96ebde68eff9ff18c85cb797
Michelle Huynh <michehuynh@yahoo.com>
2015-04-21 16:47:05 -0700

    Pretty much everything works except for giveFeedback()

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 
 var guess;
 var actualNum;
+var counter = 0;
 
 $(document).ready(function(){
 	
@@ -15,43 +16,86 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	//Every Time NEW is Clicked, new game.
   	$("nav > ul.clearfix").on('click' , 'a.new' , newGame);
-  	newGame();
-  	actualNum = pickRandomNumber();
-  	console.log(actualNum);
-  	$("#guessButton").click(function(){
-		guess = +$("#userGuess").val();
-		console.log(guess);
-	});
-	//first click prints to console the guess properly
-	//second click refreshes the page
+
+  	//When Doc is ready, newgame.
+	newGame();
 
 }); //end document ready
+
+function newGame () {
+	//clear out fields
+	counter = 0;
+	$("#userGuess").val('');
+	$("#guessList").empty(); 
+	$("span#count").text(counter);
+	$("#feedback").text('Make your Guess!');
+
+	//generate new number for game
+	actualNum = pickRandomNumber();
+  	console.log(actualNum);
+
+  	//every time guess button is clicked
+  	$("#guessButton").click(function(event){
+  		event.preventDefault();
+		guess = +$("#userGuess").val();
+		console.log(guess);
+		giveFeedback();
+		updateGuesses();
+	});
+}; //end newgame
+
+function updateGuesses () {
+	$("#guessList").append(
+		'<li>' + guess + '</i>'
+	);
+	counter++;
+	$("span#count").text(counter);
+}; //end updateguesses
 
 function pickRandomNumber () {
 	return Math.floor((Math.random() * 100) + 1);
 };
 
+
+
+/* 
+
+Issues for giveFeedback:
+-If within range for "ice cold," says ice cold [CORRECT]
+-If within range for "cold," says cold [CORRECT]
+-If it is a match, responds correctly as well. 
+-HOWEVER, if within range for warm, hot, and very hot, also says "cold."
+-How to fix?
+
+*/
+
 function giveFeedback () {
-
-};
-
-function newGame () {
-	//clear out fields
-	var counter = 0;
-	$("#userGuess").val('');
-	$("#guessList").empty(); 
-	$("span#count").text(counter);
-
-
-
-	//sometimes this works, sometimes clicking guessbutton
-	//makes the page refresh. why?
-
-
-	//check to see how close, givefeedback
-	//repeat while guess is != number
-	//when they win, givefeedback winner
-};
+	if (guess != actualNum) {
+		if (actualNum-50 >= guess || guess >= actualNum+50) {
+			$("#feedback").text('Ice cold!'); //works
+		}
+		else if (actualNum-50 < guess <= actualNum-30 || actualNum+30 <= guess < actualNum+50) {
+			$("#feedback").text('Cold!'); //not responding correctly; defaults to "Cold" for everything
+			console.log("Cold");
+		}
+		else if (actualNum-30 < guess <= actualNum-20 || actualNum+20 <= guess < actualNum+30) {
+			$("#feedback").text('Warm!'); //not responding correctly; defaults to "Cold" for everything
+			console.log("warm");
+		}
+		else if (actualNum-20 < guess <= actualNum-10 || actualNum+10 <= guess < actualNum+20) {
+			$("#feedback").text('Hot!'); //not responding correctly; defaults to "Cold" for everything
+			console.log("hot");
+		}
+		else if (actualNum-10 < guess <= actualNum-1 || actualNum+1 <= guess < actualNum+10) {
+			$("#feedback").text('Very hot!');
+			console.log("very hot");
+		}
+	}
+	else {
+		$("#feedback").text('You got it! The number is ' + actualNum + '.'); //works
+	};
+}; //end givefeedback
 
 
